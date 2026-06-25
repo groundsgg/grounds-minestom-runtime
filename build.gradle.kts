b7343d4b080f173f092759b75b2718611fe58600
Lukas Jost <lukas.jost@outlook.com>
2026-06-25 16:45:22 +0200

    fix: remove redundant dependencies repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,5 @@ allprojects {
                 password = providers.gradleProperty("github.token").get()
             }
         }
-        maven {
-            url = uri("https://maven.pkg.github.com/groundsgg/grounds-dependencies")
-            credentials {
-                username = providers.gradleProperty("github.user").get()
-                password = providers.gradleProperty("github.token").get()
-            }
-        }
     }
 }
